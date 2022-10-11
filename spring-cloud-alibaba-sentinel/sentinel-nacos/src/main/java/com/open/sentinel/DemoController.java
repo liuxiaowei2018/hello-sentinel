39a786eb54dddd3764bf5096fd76484e3ec623a2
刘校伟 <liuxiaowei@pharmakeyring.com>
2022-10-11 13:03:25 +0800

    style: sentinel-nacos 配置持久化push模式

---
+++ spring-cloud-alibaba-sentinel/sentinel-nacos/src/main/java/com/open/sentinel/DemoController.java
@@ -0,0 +1,46 @@
+package com.open.sentinel;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年10月11日 12:12
+ * @Description
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    // 测试【流量控制】
+    @GetMapping("/echo")
+    public String echo() {
+        return "echo";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+    // 测试【熔断降级】
+    @GetMapping("/sleep")
+    public String sleep() throws InterruptedException {
+        Thread.sleep(100L);
+        return "sleep";
+    }
+
+    // 测试【热点参数限流】
+    @GetMapping("/product_info")
+    @SentinelResource("demo_product_info_hot")
+    public String productInfo(Integer id) {
+        // 在方法上，添加 @SentinelResource 注解，自定义了 demo_product_info_hot 资源。
+        return "商品编号：" + id;
+
+        // ps. 在当前示例的热点规则下:
+        // 针对每个 id 对应的 http://127.0.0.1:19450/demo/product_info?id=${id} 接口，在每 60 秒内，分别允许访问 10 次。
+        // 针对 id = 1 的情况，作为特殊（例外）配置，在每 60 秒内，仅允许访问 1 次
+    }
+}
---
+++ spring-cloud-alibaba-sentinel/sentinel-nacos/src/main/java/com/open/sentinel/SentinelNacosApplication.java
@@ -0,0 +1,16 @@
+package com.open.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年10月11日 12:12
+ * @Description
+ */
+@SpringBootApplication
+public class SentinelNacosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelNacosApplication.class, args);
+    }
+}
